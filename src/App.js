337ea6e9908cdf8f24dfc966241ba4d8f565028a
Kevaunjh <90523389+Kevaunjh@users.noreply.github.com>
2023-07-08 14:17:02 -0400

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {Text, StyleSheet} from 'react-native';
 
 function App() {
   return (
-    <div></div>
+    <div><Text>I am bold and red</Text></div>
   );
 }
 
