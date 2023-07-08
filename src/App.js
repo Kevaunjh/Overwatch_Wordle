a653a2102caa20db63556fbb76b5deec694b80e3
Kevaunjh <90523389+Kevaunjh@users.noreply.github.com>
2023-07-08 15:04:00 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 function App() {
   return (
-    <div><Text>I am bold and red</Text></div>
+    <View
+    style={{
+      flexDirection: 'row',
+      height: 100,
+      padding: 20,
+    }}>
+    <View style={{backgroundColor: 'blue', flex: 0.3}} />
+    <View style={{backgroundColor: 'red', flex: 0.5}} />
+    <Text>Hello World!</Text>
+  </View>
   );
 }
 
